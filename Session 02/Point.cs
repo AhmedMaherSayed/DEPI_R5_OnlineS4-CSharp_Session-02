561968ea946cc412524f8295d172e5cfe7586c78
Ahmed Mohamed Maher Sayed <ahmedmaher.maher61@gmail.com>
2026-08-21 10:33:20 +0300

    add Refrence Value

---
+++ Session 02/Point.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Session_02
+{
+    internal class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point()
+        {
+
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
--- Session 02/Program.cs
+++ Session 02/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Reference Value
+            Point P1; // Zero Bytes Allocated at HEAP
+            P1 = new Point(1, 2);
+
+            // new 
+            // 1. Allocate Required Number of Bytes in the HEAP (Object Size + CLR Overhead Variables)
+            // 2. Initialize (Cross out) Allocated Bytes with Default Value of its Datatype
+            // 3. Call User-Defined Constructor if exists
+            // 4. Assign the Refence to the Allocated Object.
+
+            Console.WriteLine(P1.X);
+            Console.WriteLine(P1.Y);
+            Console.WriteLine(P1.GetHashCode());
+
+            Point P2 = new Point();
+
+            P2.X = P1.X;
+            P2.Y = P1.Y;
+            Console.WriteLine(P2.X);
+            Console.WriteLine(P2.Y);
+            Console.WriteLine(P2.GetHashCode());
+
+            Console.WriteLine("Is P1 = P2 ? " + P2.Equals(P1)); // False
+
+
+            P2 = P1;
+
+            Console.WriteLine("-------------------- After -------------");
+            Console.WriteLine(P1.GetHashCode());
+            Console.WriteLine(P2.GetHashCode());
+            Console.WriteLine("Is P1 = P2 ? " + P2.Equals(P1)); // True
+
+            P1.X = 10;
+            Console.WriteLine("P1.X = " + P1.X);
+            Console.WriteLine("P2.X = " + P2.X);
+            #endregion
         }
     }
 }
