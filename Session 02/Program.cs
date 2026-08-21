2887ebb1f783a1ebe44a311f62832d62165a9fa3
Ahmed Mohamed Maher Sayed <ahmedmaher.maher61@gmail.com>
2026-08-21 10:54:38 +0300

    add Fraction and Discards

--- Session 02/Program.cs
+++ Session 02/Program.cs
@@ -5,40 +5,49 @@
         static void Main(string[] args)
         {
             #region Reference Value
-            Point P1; // Zero Bytes Allocated at HEAP
-            P1 = new Point(1, 2);
+            //Point P1; // Zero Bytes Allocated at HEAP
+            //P1 = new Point(1, 2);
 
-            // new 
-            // 1. Allocate Required Number of Bytes in the HEAP (Object Size + CLR Overhead Variables)
-            // 2. Initialize (Cross out) Allocated Bytes with Default Value of its Datatype
-            // 3. Call User-Defined Constructor if exists
-            // 4. Assign the Refence to the Allocated Object.
+            //// new 
+            //// 1. Allocate Required Number of Bytes in the HEAP (Object Size + CLR Overhead Variables)
+            //// 2. Initialize (Cross out) Allocated Bytes with Default Value of its Datatype
+            //// 3. Call User-Defined Constructor if exists
+            //// 4. Assign the Refence to the Allocated Object.
 
-            Console.WriteLine(P1.X);
-            Console.WriteLine(P1.Y);
-            Console.WriteLine(P1.GetHashCode());
+            //Console.WriteLine(P1.X);
+            //Console.WriteLine(P1.Y);
+            //Console.WriteLine(P1.GetHashCode());
 
-            Point P2 = new Point();
+            //Point P2 = new Point();
 
-            P2.X = P1.X;
-            P2.Y = P1.Y;
-            Console.WriteLine(P2.X);
-            Console.WriteLine(P2.Y);
-            Console.WriteLine(P2.GetHashCode());
+            //P2.X = P1.X;
+            //P2.Y = P1.Y;
+            //Console.WriteLine(P2.X);
+            //Console.WriteLine(P2.Y);
+            //Console.WriteLine(P2.GetHashCode());
 
-            Console.WriteLine("Is P1 = P2 ? " + P2.Equals(P1)); // False
+            //Console.WriteLine("Is P1 = P2 ? " + P2.Equals(P1)); // False
 
 
-            P2 = P1;
+            //P2 = P1;
 
-            Console.WriteLine("-------------------- After -------------");
-            Console.WriteLine(P1.GetHashCode());
-            Console.WriteLine(P2.GetHashCode());
-            Console.WriteLine("Is P1 = P2 ? " + P2.Equals(P1)); // True
+            //Console.WriteLine("-------------------- After -------------");
+            //Console.WriteLine(P1.GetHashCode());
+            //Console.WriteLine(P2.GetHashCode());
+            //Console.WriteLine("Is P1 = P2 ? " + P2.Equals(P1)); // True
 
-            P1.X = 10;
-            Console.WriteLine("P1.X = " + P1.X);
-            Console.WriteLine("P2.X = " + P2.X);
+            //P1.X = 10;
+            //Console.WriteLine("P1.X = " + P1.X);
+            //Console.WriteLine("P2.X = " + P2.X);
+            #endregion
+
+            #region Fraction and Discards
+            double X = 12.5;
+            float y = 1.25F;
+
+            decimal money = 12222.2224M;
+
+            money = 1_000_000;
             #endregion
         }
     }
